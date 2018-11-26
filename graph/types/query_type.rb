ea0e4807ce96d9d8b7f63504d92c452284fa2e47
okapusta <kapuszasty@gmail.com>
2018-11-26 21:07:18 +0100

    Lamda resolver

--- graph/types/query_type.rb
+++ graph/types/query_type.rb
@@ -1,9 +1,18 @@
+require 'ostruct'
+
 module Graph
   module Types
     class QueryType < BaseType
       field :users, [Types::UserType], resolver: Resolvers::UsersResolver
       field :user, Types::UserType, resolver: Resolvers::UserResolver
       field :festival, Types::FestivalType, resolver: Resolvers::FestivalsResolver
+
+      field :lambda, [Types::UserType], resolve: -> (obj, args, ctx) {
+          [OpenStruct.new({
+            email: 'stefan@example.com',
+            handle: 'stefan'
+          })]
+        }
     end
   end
 end
